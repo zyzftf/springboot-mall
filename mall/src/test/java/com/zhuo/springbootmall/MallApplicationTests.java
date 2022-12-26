cb14f6c75702d0b607d8c59e700eb7943003cb33
Kevin Zhuo <Kevin@gmail.com>
2022-12-26 17:13:37 +0800

    初始化 Spring Boot Project

---
+++ mall/src/test/java/com/zhuo/springbootmall/MallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhuo.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
