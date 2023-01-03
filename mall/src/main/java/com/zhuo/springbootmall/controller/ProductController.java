aacd7f3c516a426d2f2db34d65182c37d748dc96
Kevin Zhuo <Kevin@gmail.com>
2023-01-03 15:56:30 +0800

    商品功能 - 實作查詢商品列表(分頁)

--- mall/src/main/java/com/zhuo/springbootmall/controller/ProductController.java
+++ mall/src/main/java/com/zhuo/springbootmall/controller/ProductController.java
@@ -11,8 +11,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.List;
 
+@Valid
 @RestController
 public class ProductController {
 
@@ -27,14 +30,20 @@ public class ProductController {
 
             // 排序 Sorting
             @RequestParam(defaultValue = "created_date") String orderBy,
-            @RequestParam(defaultValue = "desc") String sort
-            ){
+            @RequestParam(defaultValue = "desc") String sort,
+
+            // 分頁 Pagination
+            @RequestParam(defaultValue = "5") @Max(1000) @Min(0) Integer limit,
+            @RequestParam(defaultValue = "0") @Min(0) Integer offset
+    ){
 
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
         productQueryParams.setOrderBy(orderBy);
         productQueryParams.setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
 
         List<Product> productList = productService.getProducts(productQueryParams);
 
--- mall/src/main/java/com/zhuo/springbootmall/dao/impl/ProductDaoImpl.java
+++ mall/src/main/java/com/zhuo/springbootmall/dao/impl/ProductDaoImpl.java
@@ -30,6 +30,7 @@ public class ProductDaoImpl implements ProductDao {
 
         Map<String, Object> map = new HashMap<>();
 
+        // 查詢條件
         if(productQueryParams.getCategory() != null){
             sql = sql + " AND category = :category";
             map.put("category", productQueryParams.getCategory().name());
@@ -40,7 +41,13 @@ public class ProductDaoImpl implements ProductDao {
             map.put("search", "%" + productQueryParams.getSearch() + "%");
         }
 
+        // 排序
         sql = sql+" ORDER BY "+productQueryParams.getOrderBy()+" "+productQueryParams.getSort();
+
+        // 分頁
+        sql = sql+" LIMIT :limit OFFSET :offset";
+        map.put("limit", productQueryParams.getLimit());
+        map.put("offset", productQueryParams.getOffset());
 
         List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
 
